d106430bba1290f95e7a43c075cac39f9ac76835
samurrai <samurrrai.r@gmail.com>
2019-01-24 19:49:59 +0600

    Практика по исключениям

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionPractice
+{
+    class Program
+    {
+        public static Exception Work()
+        {
+            throw new Exception();
+        }
+
+        public static void Do()
+        {
+            try
+            {
+                Work();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Неизвестная ошибка");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Do();
+            Console.ReadLine();
+        }
+    }
+}
